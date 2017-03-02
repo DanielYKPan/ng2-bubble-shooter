ad5bef5627d2bdd2f685f66bbde06d2b1cb9d13a
Yaokun Pan <myron.yk.pan@gmail.com>
2017-03-02 17:32:22 +1100

    Created Bubble Class

---
+++ src/app/+game/service/bubble.ts
@@ -0,0 +1,51 @@
+/**
+ * bubble
+ */
+
+import { uuid } from './utils';
+
+export enum Color {
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Pink,
+    Cyan,
+    Grey,
+}
+
+export class Bubble {
+
+    /* Property id */
+    private id: string;
+
+    get Id(): string {
+        return this.id;
+    }
+
+    /* Property x */
+    private x: number;
+
+    get X(): number {
+        return this.x;
+    }
+
+    /* Property y */
+    private y: number;
+
+    get Y(): number {
+        return this.y;
+    }
+
+    /* Property color */
+    private color: Color;
+
+    get Color(): Color {
+        return this.color;
+    }
+
+    constructor( color: Color ) {
+        this.id = uuid();
+        this.color = color;
+    }
+}
---
+++ src/app/+game/service/utils.ts
@@ -0,0 +1,38 @@
+/**
+ * utils
+ */
+
+// Generate a random uuid value
+export function uuid() {
+    let result: string = '';
+
+    for (let i = 0; i < 32; i++) {
+        let random = Math.random() * 16 || 0;
+        if (i === 8 || i === 12 || i === 16 || i === 20) {
+            result += '-';
+        }
+        result += (i === 12 ? 4 : (i === 16 ? (random && 3 || 8) : random))
+            .toString(16);
+    }
+
+    return result;
+}
+
+// Random Shuffling An Array the Fisher-Yates (aka Knuth) Way
+export function shuffle<T>( list: T[] ): T[] {
+    let currentIndex = list.length;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        let temporaryValue = list[currentIndex];
+        list[currentIndex] = list[randomIndex];
+        list[randomIndex] = temporaryValue;
+    }
+    return list;
+}
